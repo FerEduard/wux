cc9a576a8ced3b4bb70aef6fd66b658475764a15
FerEduard <fernandoflres80@gmail.com>
2017-05-22 17:04:47 -0600

    Arbol
    Inicio de arbol de la aplicación

---
+++ arbol.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Inicio</title>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/play.js"></script>
+<script src="js/a_seccion.js"></script>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php include ("inc/menu.php");?>
+<section class="portafolio">
+<div class="container">  
+    <div class="row cuadro">
+		<div class="col-xs-12">
+        	<div class="row">
+            	<div class="col-xs-12">
+                    <img src="icons/pintura.png"  height="200px" width="200px" class="img-responsive center-block"  alt="colores">
+                    <h3 class="text-center">Colores</h3>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <img src="icons/numeros.png"  height="200px" width="200px" class="img-responsive center-block"  alt="números">
+                    <h3 class="text-center">Números</h3>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <img src="icons/mochila.png"  height="200px" width="200px" class="img-responsive center-block"  alt="personas">
+                    <h3 class="text-center">Personas</h3>
+                </div>
+            </div>
+        </div>
+
+	</div>
+    
+</div>
+</section>
+
+</body>
+
+
+</html>
